bc3f21d92ebc05394858abb7721401df408352d8
Sara-M <sara.monintja@gmail.com>
2022-10-14 21:16:46 +0800

    modication on server side. tested on apollo studio. good to go. need comment.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,6 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const bookSchema = require('./Book');
-
 const userSchema = new Schema({
   username: {
     type: String,

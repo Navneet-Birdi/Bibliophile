8ff65b6e01736bece02b2d04f900565a67374ca5
Sara-M <sara.monintja@gmail.com>
2022-10-23 15:16:15 +0800

    gitignore mod

--- server/server.js
+++ server/server.js
@@ -18,10 +18,6 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// TODO: CONFIGURE 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
